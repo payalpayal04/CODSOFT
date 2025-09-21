e5e858c426065b2520104c6587da3dfd175d8f51
payalpayal04 <payalpayal0410@gmail.com>
2025-09-21 12:13:23 +0530

    Add files via upload

---
+++ task3_Rock_paper_scissor.py
@@ -0,0 +1,55 @@
+import random
+print("***Let's play Rock-Paper-Scissors***")
+
+n=1
+c=0
+p=0
+
+def match(n,c,p):
+    print("\nChoose 1 for ROCK\nChoose 2 for PAPER\nChoose 3 for SCISSOR\n")
+    player=int(input("Enter your choie : "))
+
+    if (player == 1 or player== 2 or player== 3):
+        computer=random.randint(1,3)
+        
+        if(computer==1):
+            comp="ROCK"
+        if(computer==2):
+            comp="PAPER"
+        if(computer==3):
+            comp="SCISSOR"
+        
+        if(player<computer):
+            print(f"The computer choose {comp} and you LOST")
+            c+=1
+        elif(player>computer):
+            print(f"The computer choose {comp} and you WON")
+            p+=1
+        else:
+            print(f"The computer choose {comp} and Its's a DRAW")
+    
+    else:
+        print("INVALID CHOICE! PLEASE CHOOSE 1 OR 2 OR 3\n")
+
+    rematch=input("Do you want a rematch? (y/n) : ")
+    if(rematch=='y'):
+        n+=1
+        match(n,c,p)
+    else:
+        result(n,c,p)
+    
+
+def result(n,c,p):
+    if(n>1):
+        print("\nRESULTS")
+        print("-------")
+        for i in range (n):
+            if (p>c):
+                print(f"You have WON the series by {p}:{c}")
+            elif(c>p):
+                print(f"You have LOST the series by {p}:{c}")
+            else:
+                print(f"The match is DRAW by {p}:{c}")   
+            break
+
+match(n,c,p) 
